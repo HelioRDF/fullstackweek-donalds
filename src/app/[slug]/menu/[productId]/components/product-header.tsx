d3d7e5dbb6724ffb6a07f9f3580d11872fbdfe4c
Helio Franca <heliordf@hotmail.com>
2025-11-07 10:52:58 -0300

    feat: add router to orders

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -2,7 +2,7 @@
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -11,8 +11,10 @@ interface ProductHeaderProps {
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => router.back();
+  const handleScrollClick = () => router.push(`/${slug}/orders`);
   return (
     <div className="relative w-full min-h-[300px]">
       <Button
@@ -27,6 +29,7 @@ const ProductHeader = ({ product }: ProductHeaderProps) => {
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleScrollClick}
       >
         <ScrollTextIcon />
       </Button>
